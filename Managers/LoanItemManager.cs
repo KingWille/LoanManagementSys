43a0f60f5794f695d62a02f12b5c629520fab31c
KingWille <william19970808@gmail.com>
2024-04-04 14:50:23 +0200

    Börjat fixa om loanitemmanager

--- Managers/LoanItemManager.cs
+++ Managers/LoanItemManager.cs
@@ -9,66 +9,46 @@ namespace LoanManagementSys.Managers
 {
     internal class LoanItemManager
     {
-        private Dictionary<Product, Member> memberProductInfo;
-        private List<Product> products;
-        private Dictionary<Product, Member> returnedProduct;
+        private List<LoanItem> loanItems;
         private Random random;
-        private object lockObj;
 
         public LoanItemManager()
         {
-            memberProductInfo = new Dictionary<Product, Member>();
-            returnedProduct = new Dictionary<Product, Member>();
-            products = new List<Product>();
             random = new Random();
+            loanItems = new List<LoanItem>();
         }
         internal void LoanProduct(int memberIndex, int productIndex)
         {
-            memberProductInfo.Add(LoanSysManager.productManager.Get(productIndex), LoanSysManager.memberManager.GetMember(memberIndex));
-            products.Add(LoanSysManager.productManager.Get(productIndex));
+            loanItems.Add(new LoanItem(LoanSysManager.productManager.Get(productIndex), LoanSysManager.memberManager.GetMember(memberIndex)));
             LoanSysManager.productManager.Remove(productIndex);
-            lockObj = new object();
         }
 
         internal void ReturnProduct()
         {
-            if (memberProductInfo.Count() != 0)
-            {
-                int index = random.Next(0, products.Count());
-                Product prodtoBeReturned = products[index];
+            int index = random.Next(0, loanItems.Count());
 
-                if(!returnedProduct.ContainsKey(prodtoBeReturned))
-                {
-                    returnedProduct.Add(prodtoBeReturned, memberProductInfo[prodtoBeReturned]);
-                    LoanSysManager.productManager.Add(prodtoBeReturned);
-                    memberProductInfo.Remove(prodtoBeReturned);
-                    products.Remove(prodtoBeReturned);
-                }
-            }
+            LoanSysManager.productManager.Add(loanItems[index].Product);
+            loanItems.RemoveAt(index);
         }
         
         internal Member GetMemberInfo(Product product)
         {
-            return memberProductInfo[product];
+            
         }
 
         internal Product GetLastProductInfo()
         {
-            return products.Last();
+            
         }
 
         internal int NumberOfLoans()
         {
-            return memberProductInfo.Count();
+            
         }
 
         internal Dictionary<Product, Member> GetReturnedProduct()
         {
-            Dictionary<Product, Member> temp = returnedProduct;
-
-            returnedProduct.Clear();
             
-            return temp;
         }
     }
 }
